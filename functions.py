103556356d6c4f7038dd29247706a76f23c68b9d
yuyumi <changyu2000@gmail.com>
2023-04-15 13:38:50 -0400

    Improved code readability and added Baldwin Effect
    - Separated code into files, main should be the execution of the functions in functions.py, constants are self-explanatory.
    - Fixed naming convention and resolved a variable naming clash.
    - Added in Baldwin effect with the number of randomizations controlled by BALDWIN_ITERS constant.

---
+++ functions.py
@@ -0,0 +1,148 @@
+import math
+
+import numpy as np
+import random
+from typing import Callable, List, Optional
+from constants import *
+
+BinFunc = Callable[[int, int], int]
+
+
+class Function:
+
+    def __init__(self, f: BinFunc, g: BinFunc, h: BinFunc):
+        self.f = f
+        self.g = g
+        self.h = h
+
+    def eval(self, inputs: str) -> int:
+        # split input into four bits
+        bits = [int(inputs[i]) for i in range(0, len(inputs))]
+        x = self.f(bits[0], bits[1])
+        y = self.g(bits[2], bits[3])
+        return self.h(x, y)
+
+
+bin_funcs = [
+    # XOR
+    lambda x, y: x ^ y,
+    # AND
+    lambda x, y: x & y,
+    # OR
+    lambda x, y: x | y,
+]
+
+goals = [
+    Function(bin_funcs[0], bin_funcs[0], bin_funcs[1]),
+    Function(bin_funcs[0], bin_funcs[0], bin_funcs[2])
+]
+
+
+def generate_random_genome() -> str:
+    return ''.join(random.choice('01') for _ in range(BITS_PER_GENOME))
+
+
+def split_genome_into_genes(genome: str) -> list[list[str]]:
+    # gates are `BITS_PER_GENE` bits each
+    # output is 4 bits
+    return [
+        [genome[i], genome[i + 1:i + BITS_PER_GATE + 1], genome[i + BITS_PER_GATE + 1:i + BITS_PER_GENE]]
+        for i in range(0, len(genome) - 4, BITS_PER_GENE)
+    ] + [[genome[-4:]]]
+
+
+def genes_to_output(genes: list[list[str]], input) -> float:
+    outp_gate = int(genes[-1][0], 2)
+    return eval(outp_gate, genes, input)
+
+
+def fitness(genome: str, goal: Function) -> float:
+    # Compute losses over all possible inputs
+    # fitness = 16
+    total_losses= 0
+    for i in range(16):
+        actual = goal.eval(format(i, '04b'))
+        for _ in range(BALDWIN_ITERS):
+            genes = split_genome_into_genes(genome)
+            for j in range(len(genes)):
+                if genes[j][0] == '1':
+                    genes[j][1] = format(np.random.randint(0, 16), '04b')
+                    genes[j][2] = format(np.random.randint(0, 16), '04b')
+            est = genes_to_output(genes, format(i, '04b'))
+            total_losses += int(est != actual)
+        # fitness -= int(est != actual)
+    # fitness /= 16
+    return 1-(total_losses/(16*BALDWIN_ITERS))
+
+
+def eval(outp_gate, genes, input):
+    val: List[Optional[int]] = [None] * 16
+    vis = [False] * 16
+
+    # Returns True if successful
+    def dfs(node: int) -> bool:
+        if vis[node]:
+            return val[node] is not None
+        vis[node] = True
+        if node == 15:
+            return False
+        if node < 4:
+            val[node] = int(input[node])
+            return True
+
+        input1 = int(genes[node - 4][1], 2)
+        input2 = int(genes[node - 4][2], 2)
+        if not dfs(input1) or not dfs(input2):
+            return False
+        val[node] = not (val[input1] and val[input2])
+        return True
+
+    if not dfs(outp_gate):  # If the DFS fails
+        return -1
+
+    return val[outp_gate]
+
+
+# XOR is AND(NAND, OR)
+class Gate:
+
+    def __init__(self, input1: Optional["Gate"], input2: Optional["Gate"], id: int):
+        self.input1 = input1
+        self.input2 = input2
+        self.id = id
+
+def select_elite(pop_fitness, exp_weight=1):
+    """
+    Using the Elite selection method, best individuals
+    """
+    pop_fitness = sorted(pop_fitness, key=lambda x: x[1], reverse=True)
+    pop, fitnesses = zip(*pop_fitness)
+    fitnesses = np.array(fitnesses)
+
+    exp_fitnesses = math.e ** (exp_weight * fitnesses)
+    cum_exp_fitnesses = np.cumsum(exp_fitnesses)
+
+    selected = []
+    for _ in range(GRADUATION_SIZE):
+        r = random.uniform(0, cum_exp_fitnesses[-1])
+        idx = np.searchsorted(cum_exp_fitnesses, r)
+        selected.append(pop[idx])
+
+    return selected
+
+
+def crossover(parent1, parent2):
+    # With probability CROSSOVER_P, crossover two genes from parent1 and parent2
+    if random.random() > CROSSOVER_P:
+        return parent1, parent2
+    crossover_point = random.randint(1, len(parent1) - 1)
+    offspring1 = parent1[:crossover_point] + parent2[crossover_point:]
+    offspring2 = parent2[:crossover_point] + parent1[crossover_point:]
+    return offspring1, offspring2
+
+
+def mutate(genome: str):
+    return ''.join([
+        random.choice('01') if random.random() < MUTATION_P else genome[i]
+        for i in range(len(genome))
+    ])
--- main.py
+++ main.py
@@ -5,127 +5,10 @@ from datetime import datetime
 import numpy as np
 import random
 from typing import Callable, List, Optional
+from constants import *
+from functions import *
 
 random.seed(0)
-
-gates = 11
-bits_per_gate = 4
-bits_per_gene = 1 + 2 * bits_per_gate  # 1 bit for if the gene can be Baldwin Effect'd
-bits_per_genome = bits_per_gene * gates + 4
-
-crossover_p = 0.5
-mutation_p = 0.5 / bits_per_genome
-
-population_size = 5000
-graduation_size = 1500  # number of organisms that move to the next generation
-
-generations = 100000
-log_step = 10
-goal_change_t = 20
-
-exp_factor = 30
-
-change_goal = True
-
-BinFunc = Callable[[int, int], int]
-
-
-class Function:
-
-    def __init__(self, f: BinFunc, g: BinFunc, h: BinFunc):
-        self.f = f
-        self.g = g
-        self.h = h
-
-    def eval(self, inputs: str) -> int:
-        # split input into four bits
-        bits = [int(inputs[i]) for i in range(0, len(inputs))]
-        x = self.f(bits[0], bits[1])
-        y = self.g(bits[2], bits[3])
-        return self.h(x, y)
-
-
-bin_funcs = [
-    # XOR
-    lambda x, y: x ^ y,
-    # AND
-    lambda x, y: x & y,
-    # OR
-    lambda x, y: x | y,
-]
-
-goals = [
-    Function(bin_funcs[0], bin_funcs[0], bin_funcs[1]),
-    Function(bin_funcs[0], bin_funcs[0], bin_funcs[2])
-]
-
-
-def generate_random_genome() -> str:
-    return ''.join(random.choice('01') for _ in range(bits_per_genome))
-
-
-def split_genome_into_genes(genome: str) -> list[list[str]]:
-    # gates are `bits_per_gene` bits each
-    # output is 4 bits
-    return [
-        [genome[i], genome[i + 1:i + bits_per_gate + 1], genome[i + bits_per_gate + 1:i + bits_per_gene]]
-        for i in range(0, len(genome) - 4, bits_per_gene)
-    ] + [[genome[-4:]]]
-
-
-def genome_to_ouput(genome: str, input) -> float:
-    genes = split_genome_into_genes(genome)
-    outp_gate = int(genes[-1][0], 2)
-    return eval(outp_gate, genes, input)
-
-
-def fitness(genome: str, goal: Function) -> float:
-    # Compute losses over all possible inputs
-    fitness = 16
-    for i in range(16):
-        est = genome_to_ouput(genome, format(i, '04b'))
-        actual = goal.eval(format(i, '04b'))
-        fitness -= int(est != actual)
-    fitness /= 16
-    return fitness
-
-
-def eval(outp_gate, genes, input):
-    val: List[Optional[int]] = [None] * 16
-    vis = [False] * 16
-
-    # Returns True if successful
-    def dfs(node: int) -> bool:
-        if vis[node]:
-            return val[node] is not None
-        vis[node] = True
-        if node == 15:
-            return False
-        if node < 4:
-            val[node] = int(input[node])
-            return True
-
-        input1 = int(genes[node - 4][1], 2)
-        input2 = int(genes[node - 4][2], 2)
-        if not dfs(input1) or not dfs(input2):
-            return False
-        val[node] = not (val[input1] and val[input2])
-        return True
-
-    if not dfs(outp_gate):  # If the DFS fails
-        return -1
-
-    return val[outp_gate]
-
-
-# XOR is AND(NAND, OR)
-class Gate:
-
-    def __init__(self, input1: Optional["Gate"], input2: Optional["Gate"], id: int):
-        self.input1 = input1
-        self.input2 = input2
-        self.id = id
-
 
 inp0 = Gate(None, None, 0)
 inp1 = Gate(None, None, 1)
@@ -183,51 +66,13 @@ print(fitness(genes, goals[1]))
 #     give_inp0,
 #     give_inp0
 # )))
-# print(fitness('1' * bits_per_genome, Function(
+# print(fitness('1' * BITS_PER_GENOME, Function(
 #     give_inp0,
 #     give_inp0,
 #     give_inp0
 # )))
 
-
-def select_elite(pop_fitness, exp_weight=1):
-    """
-    Using the Elite selection method, best individuals
-    """
-    pop_fitness = sorted(pop_fitness, key=lambda x: x[1], reverse=True)
-    pop, fitnesses = zip(*pop_fitness)
-    fitnesses = np.array(fitnesses)
-
-    exp_fitnesses = math.e ** (exp_weight * fitnesses)
-    cum_exp_fitnesses = np.cumsum(exp_fitnesses)
-
-    selected = []
-    for _ in range(graduation_size):
-        r = random.uniform(0, cum_exp_fitnesses[-1])
-        idx = np.searchsorted(cum_exp_fitnesses, r)
-        selected.append(pop[idx])
-
-    return selected
-
-
-def crossover(parent1, parent2):
-    # With probability crossover_p, crossover two genes from parent1 and parent2
-    if random.random() > crossover_p:
-        return parent1, parent2
-    crossover_point = random.randint(1, len(parent1) - 1)
-    offspring1 = parent1[:crossover_point] + parent2[crossover_point:]
-    offspring2 = parent2[:crossover_point] + parent1[crossover_point:]
-    return offspring1, offspring2
-
-
-def mutate(genome: str):
-    return ''.join([
-        random.choice('01') if random.random() < mutation_p else genome[i]
-        for i in range(len(genome))
-    ])
-
-
-init_pop = [generate_random_genome() for _ in range(population_size)]
+init_pop = [generate_random_genome() for _ in range(POPULATION_SIZE)]
 init_goal = random.randint(0, 1)
 
 pop = [*init_pop]
@@ -241,13 +86,13 @@ if not os.path.exists('logs'):
     os.makedirs('logs')
 f = open(log_file, 'w+')
 
-for g in range(generations):
+for g in range(GENERATIONS):
     # anotate the population with their fitness
     pop_fitness = [(genome, fitness(genome, goals[goal])) for genome in pop]
 
     avg_fitness = sum(fitness for genome, fitness in pop_fitness) / len(pop_fitness)
 
-    if (g + 1) % log_step == 0:
+    if (g + 1) % LOG_STEP == 0:
         f.write(f'[Generation {g + 1}] Fitness: {avg_fitness} Goal: {goal}\n')
         print(f'[Generation {g + 1}] Fitness: {avg_fitness} Goal: {goal}')
 
@@ -271,5 +116,5 @@ for g in range(generations):
     # Update the population for the next generation
     pop = offspring
 
-    if change_goal and (g + 1) % goal_change_t == 0:
+    if CHANGE_GOAL and (g + 1) % GOAL_CHANGE_T == 0:
         goal = random.randint(0, 1)

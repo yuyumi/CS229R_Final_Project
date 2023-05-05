97e1dffab65d1e474e08446657cfb0a1bed0e86a
Walden Yan <waldenyan20@gmail.com>
2023-05-04 21:09:54 -0400

    better plotting again

--- plotter.py
+++ plotter.py
@@ -2,30 +2,30 @@ import re
 import matplotlib.pyplot as plt
 import numpy as np
 
-# FILE = 'logs/baldwin_fixed_run1.txt'
-# with open(FILE, 'r') as f:
-    # data = f.read()
+import sys
 
-# lines = data.split('\n')
+if len(sys.argv) < 3:
+    print("Usage: python plotter.py <beta> <change_goal>")
+    exit(1)
 
-BETA = 1
-# CHANGE_GOAL = True
+BETA = int(sys.argv[1])
+CHANGE_GOAL = bool(int(sys.argv[2]))
+
+prefix = f'baldwin_{BETA}_' if BETA > 1 else ''
+goal = f'fvg' if CHANGE_GOAL else 'fixed'
 
 # Format:
 format = re.compile(r'\[Generation (\d+)\] Fitness: (\d+\.\d+) No-Baldwin Fitness: (\d+\.\d+) Maleable: (\d+\.\d+)% Goal: (\d) Times: (\d+\.\d+)s')
-# format = re.compile(r'\[Generation (\d+)\] Fitness: (\d+\.\d+) Goal: (\d)')
 
 header = ['Generation', 'Fitness', 'Pure Fitness', '% Maleable', 'Goal', 'Time']
-# header = ['Generation', 'Fitness', 'Goal']
 
 data_map: dict[int, list] = {}
 data0_map = {}
 data1_map = {}
 
-
+# LOAD THE DATA
 for i in range(1, 11):
-    # FILE = f'aws/logs/log_baldwin_{BETA}_fvg_{i}.txt'
-    FILE = f'aws/logs/log_fvg_{i}.txt'
+    FILE = f'aws/logs/log_{prefix}{goal}_{i}.txt'
     with open(FILE, 'r', encoding='utf-8') as f:
         data = f.read()
 
@@ -40,12 +40,14 @@ for i in range(1, 11):
 
     # Only where Goal is 0
     goal0_data = np.array([d for d in data if d[4] == 0 and d[0] % 100 == 0], dtype=float)
-    goal1_data = np.array([d for d in data if d[4] == 1 and d[0] % 100 == 0], dtype=float)
     # goal0_data = np.array([d for d in data if d[2] == 0 and d[0] % 100 == 0], dtype=float)
-    # goal1_data = np.array([d for d in data if d[2] == 1 and d[0] % 100 == 0], dtype=float)
-
     data0_map[i] = goal0_data
-    data1_map[i] = goal1_data
+
+    if CHANGE_GOAL:
+        goal1_data = np.array([d for d in data if d[4] == 1 and d[0] % 100 == 0], dtype=float)
+        # goal1_data = np.array([d for d in data if d[2] == 1 and d[0] % 100 == 0], dtype=float)
+        data1_map[i] = goal1_data
+
     data_map[i] = data
 
 
@@ -53,23 +55,30 @@ def moving_average(data, window_size):
     return np.convolve(data, np.ones(window_size), 'valid') / window_size
 
 
+TITLE = (f"Fixed Goal" if not CHANGE_GOAL else f"Varying Goal") + (f" Baldwin (beta={BETA}) " if BETA > 1 else " ") + "Fitness over Generations"
+
 def drawall():
     for i in range(1, 11):
         goal0_data = data0_map[i]
-        goal1_data = data1_map[i]
+        if CHANGE_GOAL:
+            goal1_data = data1_map[i]
+        data = np.array(data_map[i])
 
-        plt.plot(goal0_data[:, 0], goal0_data[:, 1], label='Baldwin G1', color='#ff000080')
-        # plt.plot(goal0_data[:, 0], goal0_data[:, 2], label='G1', color='#ff000040')
-        plt.plot(goal1_data[:, 0], goal1_data[:, 1], label='Baldwin G2', color='#0000ff80')
-        # plt.plot(goal1_data[:, 0], goal1_data[:, 2], label='G2', color='#0000ff40')
+        if BETA > 1:
+            plt.plot(goal0_data[:, 0], goal0_data[:, 1], label='Baldwin G1', color='#ff000080')
+        plt.plot(goal0_data[:, 0], goal0_data[:, 2], label='G1', color=('#ff000040' if BETA > 1 else "#ff000080"))
 
-        # window_size = 10  # Adjust this value for the desired moving average window size
-        # maleable_moving_avg = moving_average(data[:, 3] / 100, window_size)
-        # plt.plot(data[window_size - 1:, 0], maleable_moving_avg, label='Plasticity (Moving Avg)', color='#00aa00')
+        if CHANGE_GOAL:
+            if BETA > 1:
+                plt.plot(goal1_data[:, 0], goal1_data[:, 1], label='Baldwin G2', color='#0000ff80')
+            plt.plot(goal1_data[:, 0], goal1_data[:, 2], label='G2', color='#0000ff40'if BETA > 1 else "#0000ff80")
 
+        if BETA > 1:
+            window_size = 10  # Adjust this value for the desired moving average window size
+            maleable_moving_avg = moving_average(data[:, 3] / 100, window_size)
+            plt.plot(data[window_size - 1:, 0], maleable_moving_avg, label='Plasticity (Moving Avg)', color='#00aa00')
 
-        # plt.title(f"Varying Goal Baldwin Fitness (beta={BETA}) over Generations")
-        plt.title(f"FVG Fitness over Generations")
+        plt.title(TITLE)
 
         plt.xlabel('Generations')
         plt.ylabel('Fitness')
@@ -83,10 +92,9 @@ def drawall():
         # Set y-axis limits from 0 to 1
         plt.ylim(0, 1)
 
-        # UNCOMMENT THESE TO HAVE A SEPARATE PLOT FOR EACH RUN
         # Save the plot to the specified file
-        # plt.savefig(f"aws/plots/baldwin_{BETA}_fvg_{i}.png", bbox_inches="tight")
-        plt.savefig(f"aws/plots/fvg_{i}.png", bbox_inches="tight")
+        plt.savefig(f"aws/plots/{prefix}{goal}_{i}.png", bbox_inches="tight")
+        print(f"Saved {prefix}{goal}_{i}.png")
         plt.clf()  # Clear the current figure after saving the plot
 
 def drawaverage():
@@ -108,28 +116,40 @@ def drawaverage():
         for gen in sorted_gens
         if any(d[4] == 0 for d in gen_data[gen])
     ]
-    avg1s = [
-        [gen, np.average([d[1] for d in gen_data[gen] if d[4] == 1]), np.average([d[2] for d in gen_data[gen] if d[4] == 1])]
-        for gen in sorted_gens
-        if any(d[4] == 1 for d in gen_data[gen])
-    ]
+    if CHANGE_GOAL:
+        avg1s = [
+            [gen, np.average([d[1] for d in gen_data[gen] if d[4] == 1]), np.average([d[2] for d in gen_data[gen] if d[4] == 1])]
+            for gen in sorted_gens
+            if any(d[4] == 1 for d in gen_data[gen])
+        ]
+    if BETA > 1:
+        maleables = [
+            [gen, np.average([d[3] for d in gen_data[gen]])]
+            for gen in sorted_gens
+        ]
 
     goal0_data = np.array(avg0s)
-    goal1_data = np.array(avg1s)
+    if CHANGE_GOAL:
+        goal1_data = np.array(avg1s)
+    if BETA > 1:
+        maleable_data = np.array(maleables)
 
     window_size = 20  # Adjust this value for the desired moving average window size
 
-    plt.plot(goal0_data[window_size - 1:, 0], moving_average(goal0_data[:, 1], window_size), label='Baldwin G1', color='#ff000080')
-    plt.plot(goal0_data[window_size - 1:, 0], moving_average(goal0_data[:, 2], window_size), label='G1', color='#ff000040')
-    plt.plot(goal1_data[window_size - 1:, 0], moving_average(goal1_data[:, 1], window_size), label='Baldwin G2', color='#0000ff80')
-    plt.plot(goal1_data[window_size - 1:, 0], moving_average(goal1_data[:, 2], window_size), label='G2', color='#0000ff40')
+    if BETA > 1:
+        plt.plot(goal0_data[window_size - 1:, 0], moving_average(goal0_data[:, 1], window_size), label='Baldwin G1', color='#ff000080')
+    plt.plot(goal0_data[window_size - 1:, 0], moving_average(goal0_data[:, 2], window_size), label='G1', color='#ff000040' if BETA > 1 else "#ff000080")
+    
+    if CHANGE_GOAL:
+        if BETA > 1:
+            plt.plot(goal1_data[window_size - 1:, 0], moving_average(goal1_data[:, 1], window_size), label='Baldwin G2', color='#0000ff80')
+        plt.plot(goal1_data[window_size - 1:, 0], moving_average(goal1_data[:, 2], window_size), label='G2', color='#0000ff40'if BETA > 1 else "#0000ff80")
 
-    # maleable_moving_avg = moving_average(data[:, 3] / 100, window_size)
-    # plt.plot(data[window_size - 1:, 0], maleable_moving_avg, label='Plasticity (Moving Avg)', color='#00aa00')
+    if BETA > 1:
+        plt.plot(maleable_data[window_size - 1:, 0], moving_average(maleable_data[:, 1] / 100, window_size), label='Plasticity (Moving Avg)', color='#00aa00')
 
 
-    # plt.title(f"Varying Goal Baldwin Fitness (beta={BETA}) over Generations")
-    plt.title(f"FVG Fitness over Generations")
+    plt.title('Average ' + TITLE)
 
     plt.xlabel('Generations')
     plt.ylabel('Fitness')
@@ -143,14 +163,10 @@ def drawaverage():
     # Set y-axis limits from 0 to 1
     plt.ylim(0, 1)
 
-    # UNCOMMENT THESE TO HAVE A SEPARATE PLOT FOR EACH RUN
     # Save the plot to the specified file
-    # plt.savefig(f"aws/plots/baldwin_fvg_{i}.png", bbox_inches="tight")
-    # plt.clf()  # Clear the current figure after saving the plot
+    plt.savefig(f"aws/plots/{prefix}{goal}.png", bbox_inches="tight")
+    print(f"Saved {prefix}{goal}.png")
+    plt.clf()
 
 drawall()
-# drawaverage()
-
-# UNCOMMENT THIS TO HAVE A PLOT FOR ALL RUNS
-# plt.savefig(f"aws/plots/baldwin_{BETA}_fvg.png", bbox_inches="tight")
-# plt.savefig(f"aws/plots/fvg.png", bbox_inches="tight")
+drawaverage()
